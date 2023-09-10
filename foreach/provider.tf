dc6498ebd4ec639c9215d2151c88fe60d80ed561
saikiran <saikiransiddhu26@gmail.com>
2023-09-10 23:07:12 +0530

    terraform session2

--- foreach/provider.tf
+++ foreach/provider.tf
@@ -1,9 +1,16 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = "5.15.0"
     }
+  }
+
+  backend "s3" {
+    bucket   = "roboshop-remote-state-saikiran"
+    key = "foreach-demo"
+    region = "us-east-1"
+    dynamodb_table = "roboshop-locking"
   }
 }
 
--- foreach/variables.tf
+++ foreach/variables.tf
@@ -3,4 +3,52 @@ variable "ami_id" {
     default = "ami-03265a0778a880afb" #this is the default value
   }
 
- 
+ variable "instances" {
+  type = map
+  default = {
+    MongoDB = "t3.medium"
+    MySQL = "t3.medium"
+    Redis = "t2.micro"
+    RabbitMQ = "t2.micro"
+    Catalogue = "t2.micro"
+    User = "t2.micro"
+    Cart = "t2.micro"
+    Shipping = "t2.micro"
+    Payment = "t2.micro"
+    Web = "t2.micro"
+  }
+}
+
+variable "zone_id" {
+    default = "Z00508913RJHMC5M1Z8QY"
+  }
+  variable "domain" {
+    default = "saikirandevops.cloud"
+     }
+variable "ingress" {
+  type = list 
+  default = [
+  
+{
+    from_port = 443
+    to_port = 443
+    description = "allowing PORT 80 from public"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+},
+{
+  from_port = 443
+  to_port =443
+  description = "allowing PORT 443 from public"
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+},
+{
+  from_port = 22
+  to_port = 22
+  description = "allowing PORT 22 from public"
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+  ]
+}
